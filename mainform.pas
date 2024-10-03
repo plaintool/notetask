c22138eb484e334c0f4959ed8470e800dbdd0f83
Alexander Tverskoy <astverskoy@gmail.com>
2024-10-03 10:42:23 +0000

    Memo editor added

--- mainform.pas
+++ mainform.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
-  CheckLst, ValEdit, Grids, Menus, ActnList, ComCtrls, PrintersDlgs, task;
+  CheckLst, ValEdit, Grids, Menus, ActnList, ComCtrls, PrintersDlgs, LCLIntf, LCLType, task, Types;
 
 type
 
@@ -46,8 +46,13 @@ type
     procedure aExitExecute(Sender: TObject);
     procedure aNewExecute(Sender: TObject);
     procedure aOpenExecute(Sender: TObject);
+    procedure taskGridDrawCell(Sender: TObject; aCol, aRow: integer; aRect: TRect; aState: TGridDrawState);
+    procedure taskGridHeaderSized(Sender: TObject; IsColumn: boolean; Index: integer);
+    procedure taskGridPrepareCanvas(Sender: TObject; aCol, aRow: integer; aState: TGridDrawState);
+    procedure taskGridResize(Sender: TObject);
+    procedure taskGridSelectEditor(Sender: TObject; aCol, aRow: integer; var Editor: TWinControl);
   private
-
+    procedure MemoChange(Sender: TObject);
   public
 
   end;
@@ -105,6 +110,106 @@ begin
     Tasks := TTasks.Create(TextToStringList(Content));
     Tasks.FillGrid(taskGrid);
   end;
+end;
+
+procedure TformNotetask.taskGridPrepareCanvas(Sender: TObject; aCol, aRow: integer; aState: TGridDrawState);
+var
+  MyTextStyle: TTextStyle;
+begin
+  if (aCol in [2, 3]) then
+  begin
+    MyTextStyle := taskGrid.Canvas.TextStyle;
+    MyTextStyle.SingleLine := False;
+    MyTextStyle.Wordbreak := True;
+    taskGrid.Canvas.TextStyle := MyTextStyle;
+  end;
+end;
+
+procedure TformNotetask.taskGridDrawCell(Sender: TObject; aCol, aRow: integer; aRect: TRect; aState: TGridDrawState);
+var
+  grid: TStringGrid;
+  S: string;
+  drawrect: TRect;
+  bgFill: TColor;
+begin
+  if (aCol = 1) or (aRow = 0) then exit;
+  grid := Sender as TStringGrid;
+
+  //if gdSelected in aState then
+  //  bgFill := $FFF0D0
+  //else
+  bgFill := clWhite;
+
+  grid.Canvas.Brush.Color := bgFill;
+  grid.canvas.Brush.Style := bsSolid;
+  grid.canvas.fillrect(aRect);
+
+  S := grid.Cells[ACol, ARow];
+  if Length(S) > 0 then
+  begin
+    drawrect := aRect;
+    drawrect.Inflate(-4, 0);
+    DrawText(grid.canvas.handle, PChar(S), Length(S), drawrect,
+      dt_calcrect or dt_wordbreak or dt_left);
+    if (drawrect.bottom - drawrect.top) > grid.RowHeights[ARow] then
+      grid.RowHeights[ARow] := (drawrect.bottom - drawrect.top + 2)
+    // changing the row height fires the event again!
+    else
+    begin
+      drawrect.Right := aRect.Right;
+      // grid.canvas.fillrect(drawrect);
+      DrawText(grid.canvas.handle, PChar(S), Length(S), drawrect,
+        dt_wordbreak or dt_left);
+    end;
+  end;
+
+  if gdFocused in aState then
+    grid.Canvas.DrawFocusRect(aRect);
+end;
+
+procedure TformNotetask.taskGridHeaderSized(Sender: TObject; IsColumn: boolean; Index: integer);
+begin
+  taskGridResize(Sender);
+end;
+
+procedure TformNotetask.taskGridResize(Sender: TObject);
+var
+  Rect: TRect;
+begin
+  // Получаем размеры ячейки
+  Rect := taskGrid.CellRect(taskGrid.Col, taskGrid.Row);
+
+  // Обновляем размер и позицию Memo
+  if Assigned(taskGrid.Editor) and (taskGrid.Editor is TMemo) then
+  begin
+    TMemo(taskGrid.Editor).SetBounds(Rect.Left + 4, Rect.Top, Rect.Right - Rect.Left - 5, Rect.Bottom - Rect.Top - 1);
+  end;
+end;
+
+procedure TformNotetask.MemoChange(Sender: TObject);
+begin
+  taskGrid.Cells[taskGrid.Col, taskGrid.Row] := TMemo(Sender).Text;
+end;
+
+procedure TformNotetask.taskGridSelectEditor(Sender: TObject; aCol, aRow: integer; var Editor: TWinControl);
+var
+  EditorControl: TMemo;
+  Rect: TRect;
+begin
+  if aCol = 1 then exit;
+  EditorControl := TMemo.Create(Self);
+  EditorControl.Visible := False;
+  EditorControl.Parent := taskGrid;
+  EditorControl.BorderStyle := bsNone;
+  EditorControl.OnChange := @MemoChange; // Привязываем событие
+
+  Rect := taskGrid.CellRect(aCol, aRow);
+  EditorControl.SetBounds(Rect.Left + 4, Rect.Top, Rect.Right - Rect.Left - 5, Rect.Bottom - Rect.Top - 1);
+
+  EditorControl.Text := taskGrid.Cells[aCol, aRow];
+  EditorControl.WordWrap := True; // Включение многострочного режима
+  Editor := EditorControl;
+  EditorControl.Visible := True;
 end;
 
 end.
