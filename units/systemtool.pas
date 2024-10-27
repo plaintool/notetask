d8cd5ae412e8d81c43b6c429f9654b85e240cd36
Alexander Tverskoy <astverskoy@gmail.com>
2024-10-27 10:34:25 +0000

    Moved functionality into system module

--- units/systemtool.pas
+++ units/systemtool.pas
@@ -22,6 +22,7 @@ uses
   LCLTranslator
   {$IFDEF Windows}
   ,Windows
+  ,Registry
   {$ENDIF}
   {$IFDEF Linux}
   ,Unix
@@ -33,6 +34,7 @@ uses
 
 function GetOSLanguage: string;
 function ApplicationTranslate(const Language: string; AForm: TCustomForm = nil): boolean;
+function SetFileTypeIcon(const Ext: string; IconIndex: integer): boolean;
 
 var
   Language: string;
@@ -165,6 +167,223 @@ begin
     if Assigned(Res) then
       Res.Free;
   end;
+end;
+
+function SetFileTypeIcon(const Ext: string; IconIndex: integer): boolean;
+var
+  AppPath: string;
+  {$IFDEF Windows}
+  Reg: TRegistry;
+  IconPath: string;
+  {$ENDIF}
+  {$IFDEF Linux}
+  ThemeFile: TextFile;
+  MimeFile: TextFile;
+  DesktopFile: TextFile;
+  MimeType: string;
+  UserHome: string;
+  {$ENDIF}
+  {$IFDEF MacOS}
+  PlistFile: TextFile;
+  BundlePath: string;
+  UserHome: string;
+  {$ENDIF}
+
+  {$IFDEF Linux}
+  procedure SaveIconFromResources(const ResName, OutputPath: string);
+  var
+    ResourceStream: TResourceStream;
+    FileStream: TFileStream;
+  begin
+    try
+      // Open the resource stream (ResName is the name of the resource, e.g., "icon.png")
+      ResourceStream := TResourceStream.Create(HInstance, ResName, RT_GROUP_ICON);
+      try
+        // Create the output file
+        FileStream := TFileStream.Create(OutputPath, fmCreate);
+        try
+          // Copy the content of the resource to the file
+          FileStream.CopyFrom(ResourceStream, ResourceStream.Size);
+        finally
+          FileStream.Free; // Free the file stream
+        end;
+      finally
+        ResourceStream.Free; // Free the resource stream
+      end;
+      Writeln('Icon successfully saved to: ', OutputPath); // Success message
+    except
+      on E: Exception do
+        Writeln('Error while saving the icon: ', E.Message); // Error message
+    end;
+  end;
+  {$ENDIF}
+begin
+  Result := False; // Initialize result to false
+
+  {$IFDEF Windows}
+  try
+    Reg := TRegistry.Create;
+    AppPath := Application.ExeName;
+    Reg.RootKey := HKEY_CLASSES_ROOT;
+
+    // Create a key for the file extension
+    if Reg.OpenKey(Ext, True) then
+    begin
+      Reg.WriteString('', 'notetask'); // Assign the class name
+      Reg.CloseKey;
+    end;
+
+    // Create a key for Notetask
+    if Reg.OpenKey('notetask\DefaultIcon', True) then
+    begin
+      IconPath := Format('%s,%d', [AppPath, IconIndex]);
+      Reg.WriteString('', IconPath); // Set the icon path
+      Reg.CloseKey;
+    end;
+
+    // Create a key for opening the file
+    if Reg.OpenKey('notetask\shell\open\command', True) then
+    begin
+      Reg.WriteString('', Format('"%s" "%%1"', [AppPath])); // Command to open the file
+      Reg.CloseKey;
+    end;
+
+    Result := True; // Set result to true if all operations succeeded
+  except
+    on E: Exception do
+    begin
+      // Handle any exceptions here (optional: log the error)
+    end;
+  end;
+
+  Reg.Free; // Free the registry object
+  {$ENDIF}
+
+  {$IFDEF Linux}
+  try
+    AppPath := Application.ExeName;
+    MimeType := 'application/x-notetask';
+    UserHome := GetEnvironmentVariable('HOME');
+
+    // Create necessary directories if they do not exist
+    ForceDirectories(UserHome + '/.local/share/mime/packages/');
+    ForceDirectories(UserHome + '/.local/share/applications/');
+    ForceDirectories(UserHome + '/.local/share/icons/notetask');
+
+    SaveIconFromResources('TASKDOC', UserHome + '/.local/share/icons/notetask/taskdoc.ico');
+
+    // Create the index.theme file for the icon theme
+    AssignFile(ThemeFile, UserHome + '/.local/share/icons/notetask/index.theme');
+    Rewrite(ThemeFile);
+    Writeln(ThemeFile, '[Icon Theme]');
+    Writeln(ThemeFile, 'Name=notetask');
+    Writeln(ThemeFile, 'Comment=Icons for Notetask application');
+    Writeln(ThemeFile, 'Inherits=default'); // Optionally specify a parent icon theme
+    Writeln(ThemeFile, 'Directories=notetask');
+    Writeln(ThemeFile, '');
+    Writeln(ThemeFile, '[notetask]');
+    Writeln(ThemeFile, 'Size=16;32;48;64;128;256'); // Specify available icon sizes
+    Writeln(ThemeFile, 'Type=Fixed'); // Type can be Fixed or Scalable
+    Writeln(ThemeFile, 'Icon=taskdoc.ico'); // Name of the ICO file
+    CloseFile(ThemeFile);
+
+    // Create a .xml file for MIME type
+    AssignFile(MimeFile, UserHome + '/.local/share/mime/packages/x-notetask.xml');
+    Rewrite(MimeFile);
+    Writeln(MimeFile, '<?xml version="1.0" encoding="UTF-8"?>');
+    Writeln(MimeFile, '<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">');
+    Writeln(MimeFile, '  <mime-type type="', MimeType, '">');
+    Writeln(MimeFile, '    <comment>Notetask file</comment>');
+    Writeln(MimeFile, '    <glob pattern="*', Ext, '"/>');
+    Writeln(MimeFile, '    <icon name="notetask/taskdoc.ico"/>');
+    Writeln(MimeFile, '  </mime-type>');
+    Writeln(MimeFile, '</mime-info>');
+    CloseFile(MimeFile);
+
+    // Create a .desktop file
+    AssignFile(DesktopFile, UserHome + '/.local/share/applications/x-notetask.desktop');
+    Rewrite(DesktopFile);
+    Writeln(DesktopFile, '[Desktop Entry]');
+    Writeln(DesktopFile, 'Name=Notetask');
+    Writeln(DesktopFile, 'Exec=', AppPath, ' %f');
+    //Writeln(DesktopFile, 'Icon=notetask'); // Specify the icon name or full path if necessary
+    Writeln(DesktopFile, 'Type=Application');
+    Writeln(DesktopFile, 'MimeType=', MimeType);
+    CloseFile(DesktopFile);
+
+    // Update MIME database
+    if (FpSystem('xdg-mime install --mode user ' + UserHome + '/.local/share/mime/packages/x-notetask.xml') = 0) and
+       (FpSystem('update-mime-database ' + UserHome + '/.local/share/mime') = 0) and
+       (FpSystem('xdg-desktop-menu install --mode user ' + UserHome + '/.local/share/applications/x-notetask.desktop') = 0) and
+       (FpSystem('gtk-update-icon-cache '+UserHome+'/.local/share/icons/notetask/ -f') = 0) then
+    begin
+      Result := True; // Indicate success
+    end
+    else
+    begin
+      // Log error or handle failure
+      Writeln('Error updating MIME database or desktop menu.');
+    end;
+  except
+    on E: Exception do
+    begin
+      Writeln('Error: ', E.Message); // Print the error message for diagnosis
+      Exit;
+    end;
+  end;
+  {$ENDIF}
+
+  {$IFDEF MacOS}
+  try
+    AppPath := Application.ExeName;
+    UserHome := GetEnvironmentVariable('HOME');
+    BundlePath := UserHome + '/Library/Application Support/Notetask'; // Define a bundle path for the app
+
+    // Create directory for app support if it does not exist
+    if not DirectoryExists(BundlePath) then
+      CreateDir(BundlePath);
+
+    // Create a .plist file for the application
+    AssignFile(PlistFile, BundlePath + '/com.example.notetask.plist'); // Adjust the bundle identifier as needed
+    Rewrite(PlistFile);
+    Writeln(PlistFile, '<?xml version="1.0" encoding="UTF-8"?>');
+    Writeln(PlistFile, '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">');
+    Writeln(PlistFile, '<plist version="1.0">');
+    Writeln(PlistFile, '<dict>');
+    Writeln(PlistFile, '  <key>CFBundleTypeDeclarations</key>');
+    Writeln(PlistFile, '  <array>');
+    Writeln(PlistFile, '    <dict>');
+    Writeln(PlistFile, '      <key>CFBundleTypeName</key>');
+    Writeln(PlistFile, '      <string>Notetask file</string>');
+    Writeln(PlistFile, '      <key>CFBundleTypeRole</key>');
+    Writeln(PlistFile, '      <string>Editor</string>');
+    Writeln(PlistFile, '      <key>LSItemContentTypes</key>');
+    Writeln(PlistFile, '      <array>');
+    Writeln(PlistFile, '        <string>public.data</string>'); // Adjust the content type as needed
+    Writeln(PlistFile, '      </array>');
+    Writeln(PlistFile, '      <key>LSHandlerRank</key>');
+    Writeln(PlistFile, '      <string>Owner</string>');
+    Writeln(PlistFile, '      <key>CFBundleTypeIconFile</key>');
+    Writeln(PlistFile, '      <string>your_icon.icns</string>'); // Replace with your icon file
+    Writeln(PlistFile, '    </dict>');
+    Writeln(PlistFile, '  </array>');
+    Writeln(PlistFile, '</dict>');
+    Writeln(PlistFile, '</plist>');
+    CloseFile(PlistFile);
+
+
+    // Associate the file extension with the application
+    FpSystem(Format('duti -s com.example.notetask .%s public.data', [Ext])); // Adjust the bundle identifier as needed
+
+    Result := True; // Set result to true if all operations succeeded
+  except
+    on E: Exception do
+    begin
+      // Handle file creation error
+      Exit;
+    end;
+  end;
+  {$ENDIF}
 end;
 
 end.
