25ad9b98af1244a20bd2c62c922ac70466cdbcdf
Alexander Tverskoy <astverskoy@gmail.com>
2025-06-24 06:56:38 +0000

    Cleaned up logic to preserve task order

--- installer/debsetup/makedeb.cmd
+++ installer/debsetup/makedeb.cmd
@@ -2,7 +2,7 @@ tar -czf control.tar.gz -C CONTROL .
 
 tar -czf data.tar.gz -C DATA .
 
-ar rcs notetask.deb debian-binary control.tar.gz data.tar.gz
+ar r notetask.deb debian-binary control.tar.gz data.tar.gz
 
 del control.tar.gz
 del data.tar.gz
